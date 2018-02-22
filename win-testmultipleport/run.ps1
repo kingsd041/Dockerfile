7aa9134c7bad498bc6672ce5577a6a4193ebd5e8
kingsd041 <nicholas_ksd@hotmail.com>
2018-02-22 20:47:49 +0800

    Modify the nginx.conf directory.

--- win-testmultipleport/run.ps1
+++ win-testmultipleport/run.ps1
@@ -14,8 +14,6 @@ mkdir -p C:\nginx-1.9.13\html\91
 if($CONTAINER_NAME -eq $NULL){
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\service.html'
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\81\service.html'
-    "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\81\service.html'
-    "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\81\service.html'
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\82\service.html'
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\83\service.html'
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\84\service.html'
@@ -25,7 +23,7 @@ if($CONTAINER_NAME -eq $NULL){
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\88\service.html'
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\89\service.html'
     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\90\service.html'
-	"{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\91\service.html'
+    "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\91\service.html'
 }
 Else{
     echo ${CONTAINER_NAME} > 'C:\nginx-1.9.13\html\service.html'
