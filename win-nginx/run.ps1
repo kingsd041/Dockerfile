f29811118cc208fb6f98cd4ca4a9d6da61a7ef9b
hailong <nicholas_ksd@hotmail.com>
2018-02-24 16:06:57 +0800

    Update run.ps1

--- win-nginx/run.ps1
+++ win-nginx/run.ps1
@@ -1,8 +1,8 @@
 if($CONTAINER_NAME -eq $NULL){
-     "{0}" -f $env:COMPUTERNAME > 'C:\nginx-1.9.13\html\name.html'
+    "{0}" -f $env:COMPUTERNAME | Out-File -Encoding "Default" 'C:\nginx-1.9.13\html\name.html'
 }
 Else{
-    echo ${CONTAINER_NAME} > 'C:\nginx-1.9.13\html\name.html'
+    "{0}" -f $env:COMPUTERNAME | Out-File -Encoding "Default" 'C:\nginx-1.9.13\html\name.html'
 }
 
 Invoke-Expression "$args"
